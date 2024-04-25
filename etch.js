2b3bc2925861e8944e34e87505e0e1f52643eeaf
Ryan <163334703+Skyyry@users.noreply.github.com>
2024-04-25 11:48:39 +0100

    cleanup

--- etch.js
+++ etch.js
@@ -1,7 +1,10 @@
 const container = document.querySelector('#container');
 
+let gridCount = 256;
 
-for (let i = 0; i < 256; i++) {
+
+
+for (let i = 0; i < gridCount; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
     container.appendChild(square);
@@ -13,7 +16,14 @@ for (let i = 0; i < 256; i++) {
     square.addEventListener("mouseout", function() {
         square.style.backgroundColor = "blue";
     })
-  }
+}
 
 
-  
+
+function userPrompt() {
+     prompt("Enter grid size")
+}
+
+document.getElementById('setgrid').addEventListener('click', function() {
+    gridCount = userPrompt();
+});
